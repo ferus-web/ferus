4106cb5a92ef737dcc22a306b1417574e48ff086
xTrayambak <xtrayambak@gmail.com>
2023-07-12 12:41:36 +0530

    (fix) build utils now work well

--- src/utils/build.nim
+++ src/utils/build.nim
@@ -3,6 +3,9 @@
 
   This code is licensed under the MIT license
 ]#
+
+const NimblePkgVersion {.strdefine.} = "0.1.0"
+
 type
   CompilerType* = enum
     ctGcc
@@ -17,15 +20,44 @@ type
     bfMac
     bfBsd
 
-proc getVersion*: string =
-  "0.1.0"
+proc `$`*(ct: CompilerType): string =
+  case ct:
+    of ctGcc: "GNU Compiler Collection"
+    of ctClang: "Clang"
+    of ctMsvc: "Microsoft Visual C/C++ Compiler"
+    of ctCc: "Generic C Compiler"
 
-proc getCompilerType =
-  # TODO(xTrayambak) maybe this should be fixed.
-  return
+proc getVersion*: string = 
+  NimblePkgVersion
+
+proc getCompilerType*: CompilerType =
+  when defined(gcc):
+    return ctGcc
+  
+  when defined(msvc):
+    return ctMsvc
+
+  when defined(clang):
+    return ctClang
+
+  # Generic C compiler
+  return ctCc
+
+proc getArchitecture*: string =
+  hostCPU
+
+proc getHostOS*: string =
+  hostOS
+
+proc getCompileDate*: string =
+  CompileDate
 
 proc getBinFormat =
-  # TODO(xTrayambak) maybe this one too... ^_^
+  when defined(win32): return bfWin32
+  when defined(win64): return bfWin64
+  when defined(linux): return bfLinux
+  when defined(mac): return bfMac
+  when defined(freebsd) or defined(openbsd) or defined(netbsd) or defined(bsd): return bfBsd
   return
 
 proc isDebugBuild*: bool =
