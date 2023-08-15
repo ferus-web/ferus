723efaebcaf1c69982924463b3f0623472e6253f
xTrayambak <xtrayambak@gmail.com>
2023-08-15 15:05:45 +0530

    (add) scripts for some stuff

---
+++ scripts/refactor/replace.py
@@ -0,0 +1,14 @@
+file_name = input("File name >> ")
+to_replace = input("Replaces >> ")
+to_replace_with = input("With >> ")
+
+file_tmp = open(file_name, "r")
+data = file_tmp.read()
+file_tmp.close()
+
+with open(file_name, "w") as file:
+    file.write(
+        data.replace(to_replace, to_replace_with)
+    )
+
+print("Done. Replaced {} with {} in file {}".format(to_replace, to_replace_with, file_name))
