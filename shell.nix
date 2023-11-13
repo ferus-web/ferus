8afc0819adfb6ec7477e112934027c9a9545742b
xTrayambak <xtrayambak@gmail.com>
2023-11-13 11:19:24 +0530

    (update) update shell.nix

--- shell.nix
+++ shell.nix
@@ -6,12 +6,15 @@ mkShell {
     xorg.libX11.dev
     xorg.libXext
     xorg.libXext.dev
+    openssl
+    openssl.dev
     libGL
   ];
 
   LD_LIBRARY_PATH = lib.makeLibraryPath [
     libGL
     xorg.libXext.dev
+    openssl.dev
     xorg.libX11.dev
   ];
 }
