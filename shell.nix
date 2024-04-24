2611fe0176af89bc03f843f63e451d5684c44c98
xTrayambak <xtrayambak@gmail.com>
2024-04-24 07:07:59 +0530

    (add) add glfw to Nix shell

--- shell.nix
+++ shell.nix
@@ -6,14 +6,24 @@ mkShell {
     xorg.libX11.dev
     xorg.libXext
     xorg.libXext.dev
+
+    wayland.dev
+    wayland-protocols
+    wayland-scanner.dev
+    libxkbcommon.dev
     libseccomp.dev
     libGL
+    glfw
   ];
 
   LD_LIBRARY_PATH = lib.makeLibraryPath [
     libGL
     xorg.libXext.dev
     xorg.libX11.dev
+    wayland.dev
+    libxkbcommon.dev
+    wayland-scanner.dev
+    glfw
     libseccomp.dev
   ];
 }
