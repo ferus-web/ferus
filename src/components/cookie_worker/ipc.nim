966381cb874f04bb173b031ee7f028b897afe7d4
xTrayambak <xtrayambak@disroot.org>
2024-11-17 16:22:12 +0530

    (rm) components: remove cookie worker

--- src/components/cookie_worker/ipc.nim
+++
@@ -1,8 +0,0 @@
-import std/options
-import ../web/cookie/parsed_cookie
-import ferus_ipc/shared
-
-type
-  CookieWorkerStore* = ref object
-    kind: FerusMagic = feCookieWorkerStore
-    cookie*: ParsedCookie
