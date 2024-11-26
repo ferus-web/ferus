b441a0160d5391a5a0ba8f963aa2a06adeee8fa4
xTrayambak <xtrayambak@disroot.org>
2024-11-26 16:37:03 +0530

    (add) add code for layout v2

---
+++ src/components/layout_v2/block_box.nim
@@ -0,0 +1,5 @@
+import pkg/[vmath]
+
+type
+  BlockBox* = object
+
---
+++ src/components/layout_v2/node.nim
@@ -0,0 +1,44 @@
+import std/[logging, options]
+import pkg/[vmath]
+import ../components/parsers/html/document
+
+type
+  LayoutMode* {.pure.} = enum
+    Default
+    AllPossibleLineBreaks
+    OnlyRequiredLineBreaks
+
+  PaintPhase* {.pure.} = enum
+    Background
+    Border
+    Foreground
+    FocusOutline
+    Overlay
+
+  HitTestResultInternalPosition* = enum
+    None
+    Before
+    Inside
+    After
+  
+  HitTestResult* = object
+    node*: ref Node
+    indexInNode*: int = 0
+    internalPosition*: HitTestResultInternalPosition = None
+
+  HitTestType* {.pure.} = enum
+    Exact
+    TextCursor
+
+  NodeWithStyle* = object of Node
+
+  NodeWithStyleAndBoxModelMetrics* = object of NodeWithStyle
+
+  Box* = object of NodeWithStyleAndBoxModelMetrics
+
+  BlockBox* = object of Box
+
+  Node* = object of RootObj
+    domNode*: Option[HTMLElement]
+    document*: HTMLDocument
+    containingBox*: BlockBox
