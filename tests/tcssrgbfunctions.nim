919e560b824c9a698f25862ba28ded9e4bf1cb88
xTrayambak <xtrayambak@disroot.org>
2025-03-04 12:55:32 +0530

    add: css: `rgb()` and `rgba()` functions

---
+++ tests/tcssrgbfunctions.nim
@@ -0,0 +1,21 @@
+import std/[logging]
+import components/style/functions
+import components/parsers/css/types
+import colored_logger, pretty
+
+addHandler(newColoredLogger())
+
+var val = function(
+  "rgba",
+  @[
+    number 255,
+    number 230,
+    number 80,
+    number 32
+  ]
+)
+
+print val
+
+let converted = evaluateRGBFunction(val)
+print converted
